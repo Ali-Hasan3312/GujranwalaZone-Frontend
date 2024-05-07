9eaf9faffa4167fab8dd990445a5eec6b5603101
Ali-Hasan3312 <alihasan331229@gmail.com>
2024-05-07 22:20:57 +0500

    add Coupon generator app and toss app in apps component

--- src/pages/apps/coupon.tsx
+++ src/pages/apps/coupon.tsx
@@ -1,14 +1,111 @@
+import { FormEvent, useEffect, useState } from "react";
 import AdminSideBar from "../../components/adminSideBar"
+const allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const allNumbers = "1234567890";
+const allSymbols = "!@#$%^&*()_+";
 
 const Coupon = () => {
+  const [size, setSize] = useState<number>(8);
+  const [prefix, setPrefix] = useState<string>("");
+  const [includeNumbers, setIncludeNumbers] = useState<boolean>(false);
+  const [includeCharacters, setIncludeCharacters] = useState<boolean>(false);
+  const [includeSymbols, setIncludeSymbols] = useState<boolean>(false);
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const [coupon, setCoupon] = useState<string>("");
+
+  const copyText = async (coupon: string) => {
+    await window.navigator.clipboard.writeText(coupon);
+    setIsCopied(true);
+  };
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!includeNumbers && !includeCharacters && !includeSymbols)
+      return alert("Please Select One At Least");
+
+    let result: string = prefix || "";
+    const loopLength: number = size - result.length;
+
+    for (let i = 0; i < loopLength; i++) {
+      let entireString: string = "";
+      if (includeCharacters) entireString += allLetters;
+      if (includeNumbers) entireString += allNumbers;
+      if (includeSymbols) entireString += allSymbols;
+
+      const randomNum: number = ~~(Math.random() * entireString.length);
+      result += entireString[randomNum];
+    }
+
+    setCoupon(result);
+  };
+
+  useEffect(() => {
+    setIsCopied(false);
+  }, [coupon]);
+
   return (
-    <div>
+    <div className='grid grid-cols-[20%_80%] gap-4 h-screen pr-4 bg-gray-100'>
         <AdminSideBar />
-        <main>
-            <h1>Stopwatch</h1>
-            <section>
-                
-            </section>
+        <main className=" bg-white p-16">
+            <h1 className=" mb-12 ml-8 font-bold text-2xl">Coupon</h1>
+            <section className=" flex flex-col items-center justify-center gap-8 h-full">
+          <form className="coupon-form" onSubmit={submitHandler}>
+            <input className=" p-4 outline-none rounded"
+              type="text"
+              placeholder="Text to include"
+              value={prefix}
+              onChange={(e) => setPrefix(e.target.value)}
+              maxLength={size}
+            />
+
+            <input className=" p-4 outline-none rounded"
+              type="number"
+              placeholder="Coupon Length"
+              value={size}
+              onChange={(e) => setSize(Number(e.target.value))}
+              min={8}
+              max={25}
+            />
+
+            <fieldset className=" p-4 flex-wrap rounded flex flex-row items-center justify-center gap-0">
+              <legend>Include</legend>
+
+              <input
+                type="checkbox"
+                checked={includeNumbers}
+                onChange={() => setIncludeNumbers((prev) => !prev)}
+              />
+              <span className=" ml-1 mr-4 text-sm font-light">Numbers</span>
+
+              <input
+                type="checkbox"
+                checked={includeCharacters}
+                onChange={() => setIncludeCharacters((prev) => !prev)}
+              />
+              <span className=" ml-1 mr-4 text-sm font-light">Characters</span>
+
+              <input
+                type="checkbox"
+                checked={includeSymbols}
+                onChange={() => setIncludeSymbols((prev) => !prev)}
+              />
+              <span className=" ml-1 mr-4 text-sm font-light">Symbols</span>
+            </fieldset>
+            <button type="submit" className="btngrid font-bold text-lg w-full p-4 border-none text-white cursor-pointer my-8 rounded-lg bg-blue-500">Generate</button>
+          </form>
+
+          {coupon && (
+            <code className=" relative text-lg tracking-[2px] cursor-pointer ">
+              {coupon}{" "}
+              <span className=" opacity-0 text-center hover:opacity-100 h-full w-full top-0 left-0 absolute rounded bg-black text-white"
+               onClick={() => copyText(coupon)}>
+                {isCopied ? "Copied" : "Copy"}
+              </span>{" "}
+            </code>
+          )}
+        </section>
         </main>
     </div>
   )
--- src/pages/apps/toss.tsx
+++ src/pages/apps/toss.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react"
 import AdminSideBar from "../../components/adminSideBar"
 
 const Toss = () => {
+  const [angle, setAngle] = useState<number>(0)
+  const flipCoin = ()=>{
+    if(Math.random() > 0.5){
+      setAngle((prev)=> prev + 180);
+    }
+    else{
+      setAngle((prev)=> prev + 360)
+    }
+  }
   return (
-    <div>
+    <div className='grid grid-cols-[20%_80%] gap-4 h-screen pr-4 bg-gray-100'>
         <AdminSideBar />
-        <main>
-            <h1>Stopwatch</h1>
-            <section>
-                
+        <main className=" bg-white p-16">
+            <h1 className=" mb-12 ml-8 font-bold text-2xl">Toss</h1>
+            <section className=" flex flex-col items-center  gap-8 h-full">
+                <article className="tosscoin m-8 h-60 w-60 relative cursor-pointer " onClick={flipCoin} style={{transform: `rotateY(${angle}deg)`}}>
+                  <div className="first rounded-full h-full w-full absolute grid place-items-center bg-no-repeat" >
+                  
+                  </div>
+                  <div className="second rounded-full h-full w-full absolute grid place-items-center bg-no-repeat"></div>
+                </article>
             </section>
         </main>
     </div>
